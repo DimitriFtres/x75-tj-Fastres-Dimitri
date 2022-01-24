1c45ab6af814f1588f9404dcfe5e70287a242fd8
DimitriFtres <dimitrifastres@gmail.com>
2022-01-24 21:34:05 +0100

    Les 10 premiers exercices

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'info',
+    loadChildren: () => import('./info-module/info-module.module').then(n => n.InfoModuleModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyIdentityComponent } from './my-identity/my-identity.component';
+import { MyDescriptionComponent } from './my-description/my-description.component';
+import { InfoPageComponent } from './info-page/info-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import {InfoModuleModule} from "./info-module/info-module.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyIdentityComponent,
+    MyDescriptionComponent,
+    InfoPageComponent,
+    ContactPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InfoModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
